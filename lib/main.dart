8b278fc15f31a5f8aa2368f8209dda54e1264e6a
Vibhusha Gangulel <vibhushananayakkara@gmail.com>
2026-03-07 14:34:21 +0530

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/login_screen.dart';
 import 'screens/dashboard_screen.dart';
 import 'screens/assessment_screen.dart';
 import 'screens/result_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const OcularCareApp());
 }
 
